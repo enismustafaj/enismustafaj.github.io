20d0fe8356759669ced0707c9f0538a7befa17f5
emustafaj <enismustafaj@gmail.com>
2025-08-17 21:13:24 +0200

    Initial Commit with Zola

--- copy_build.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-if os.path.isdir("docs"):
-    shutil.rmtree("./docs", ignore_errors=True)
-
-shutil.copytree("./build/web", "./docs/")
-shutil.rmtree("./build", ignore_errors=True)
